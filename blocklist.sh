ba062e43d22d1821f8b9414d30aa508184dffb51
zhouzhiguo <617515061@qq.com>
2022-05-20 10:49:01 +0800

    up

--- blocklist.sh
+++ blocklist.sh
@@ -16,6 +16,7 @@ curl --request POST $funcurl \
 	"fuzzylist": `cat $config|jq '.fuzzylist'`,
 	"whitelist": `cat $config|jq '.whitelist'`
 }"
+echo "判断接口是否调用成功"
 [ $? -eq 0 ] && echo success ||  echo fail 
 #-d `cat ${config}`
 #-d '{
