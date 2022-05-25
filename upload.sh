a54943422031cfa8a7f790469b9e4e2666d16d75
zhouzhiguo <617515061@qq.com>
2022-05-25 11:28:48 +0800

    up

--- upload.sh
+++ upload.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-funcurl=https://devsl-api.bitkinetic.com/api/v2/block/up
-config=./jieshun/config.json
-# cat $config
-# curl -k -H "Content-Type:application/json;charset=utf-8" $funcurl -X POST -d "${namejson}" >> nameOutput.txt
+funcurl=$funcurl
+config=./$pj_name/config.json
 blocklist=`cat $config|jq '.blocklist'`
 fuzzylist=`cat $config|jq '.fuzzylist'`
 whitelist=`cat $config|jq '.whitelist'`
-echo "==============================="
-
 request=`curl --request POST $funcurl \
 --header "Content-Type: application/json" \
 --header "Authorization: $ACCESS_TOKEN" \
@@ -17,16 +13,12 @@ request=`curl --request POST $funcurl \
         \"fuzzylist\": $fuzzylist,
         \"whitelist\": $whitelist
 }"`
-echo "=============打印请求返回结果============="
 echo $request
-echo "=============打印请求返回码============="
-# request_code=`echo $request|awk -F ':' '{print $2}'|awk -F ',' '{print $1}'`
 request_code=`echo $request|awk -F '[:,]' '{print $2}'`
 echo "request_code: $request_code"
 expr $request_code + 1 >  /dev/null 2>&1
 if [ $? -eq 0 ] && [ $request_code -eq 200 ];then
-	echo "api request is success"
+        echo "api request is success"
 else
-	 echo "api request is fail"
+        echo "api request is fail"
 fi
-
